d8619b6dc131d21b428917b988012949642ab6c9
DaviMCaldeira <davi.caldeira@vonbraunlabs.com.br>
2024-04-10 19:54:50 -0300

    feat: callback to process

--- subscriber.py
+++ subscriber.py
@@ -7,35 +7,28 @@ load_dotenv()
 
 class Subscriber():
 
-    def __init__(self):
+    def __init__(self, on_message_callback):
 
         self.broker_address = os.getenv('BROKER_ADDRESS')
         self.port = int(os.getenv('PORT'))
         self.user = os.getenv('MQTT_USER')
         self.password = os.getenv('MQTT_PASSWORD')
         self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        self.on_message_callback = on_message_callback  
 
-    
     def define_topic(self, topic):
         self.topic = topic
     
     def listen_message(self):
         
-        def on_message( nt, userdata, message):
+        def on_message(client, userdata, message):
             print(f"Received message '{message.payload.decode()}' on topic '{message.topic}'")
+            self.on_message_callback(message)  # Chama a função de callback
 
-        # Configura credenciais
         if (self.user != ""):
             self.client.username_pw_set(self.user, password=self.password)
 
-        # Define os callbacks
         self.client.on_message = on_message
-
-        # Conecta ao broker
         self.client.connect(self.broker_address, port=self.port)
-
-        # Assina o tópico
         self.client.subscribe(self.topic)
-
-        # Inicia o loop para esperar por mensagens
         self.client.loop_forever()
