b61aa2422ecd0fbb59f45c272c5e4e0b88c33e0a
DaviMCaldeira <davi.caldeira@vonbraunlabs.com.br>
2024-06-12 21:59:00 -0300

    feat: request/insert rfid for flask

--- Engine/register_manage.py
+++ Engine/register_manage.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from models import RegisteredGarbage
+from models import RFID
 
 def add_id_to_register(session: Session, new_id: str):
     if not session.query(RegisteredGarbage).filter(RegisteredGarbage.value == new_id).first():
@@ -17,8 +18,27 @@ def remove_id_from_register(session: Session, removal_id: str):
         return True
     return False
 
+def add_rfid_to_register(session: Session, new_id: str):
+    if not session.query(RFID).filter(RFID.value == new_id).first():
+        new_rfid = RFID(value=new_id)
+        session.add(new_rfid)
+        session.commit()
+        return True
+    return False
+
+def remove_rfid_from_register(session: Session, removal_id: str):
+    rfid = session.query(RFID).filter(RFID.value == removal_id).first()
+    if rfid:
+        session.delete(rfid)
+        session.commit()
+        return True
+    return False
+
 def is_id_registered(session: Session, check_id: str):
     return session.query(RegisteredGarbage).filter(RegisteredGarbage.value == check_id).first() is not None
 
 def return_ids(session: Session):
     return [registered_garbage.value for registered_garbage in session.query(RegisteredGarbage).all()]
+
+def return_rfids(session: Session):
+    return [rfid.value for rfid in session.query(RFID).all()]
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from publisher import Publisher
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine, Base
 from models import RFID, RegisteredGarbage, Log
-from Engine.register_manage import add_id_to_register, remove_id_from_register, is_id_registered, return_ids
+from Engine.register_manage import add_id_to_register, remove_id_from_register, is_id_registered, return_ids, return_rfids, add_rfid_to_register, remove_rfid_from_register
 
 load_dotenv()
 
@@ -70,6 +70,28 @@ def on_register_message(session, message, publisher):
         publisher.send_message(data)
         print(f"Message {data} SENT to {os.getenv('REQUEST_TOPIC')}.")
 
+def on_register_rfid_message(session, message, publisher):
+    data = json.loads(message.payload)
+    command = data["command"]
+    id = data["id"]
+
+    if command == "add":
+        if add_rfid_to_register(session, id):
+            print(f"ID {id} added successfully.")
+        else:
+            print(f"ID {id} is already registered.")
+    elif command == "remove":
+        if remove_rfid_from_register(session, id):
+            print(f"ID {id} removed successfully.")
+        else:
+            print(f"ID {id} is not registered.")
+    elif command == "request":
+        registeredIds = return_rfids(session)
+        data = json.dumps(registeredIds)
+        publisher.define_topic(os.getenv("REQUEST_RFID_TOPIC"))
+        publisher.send_message(data)
+        print(f"Message {data} SENT to {os.getenv('REQUEST_RFID_TOPIC')}.")
+
 def setup_subscriber(topic, on_message_callback):
     subscriber = Subscriber(on_message_callback)
     subscriber.define_topic(topic)
@@ -85,22 +107,27 @@ def main():
     publisher1 = Publisher()
     publisher2 = Publisher()
     publisher3 = Publisher()
+    publisher4 = Publisher()
 
     rfid_subscriber = setup_subscriber(os.getenv("RFID_TOPIC"), lambda msg: on_rfid_message(session, msg, publisher1))
     ultrasonic_subscriber = setup_subscriber(os.getenv("ULTRASONIC_TOPIC"), lambda msg: on_ultrasonic_message(session, msg, publisher2))
     register_subscriber = setup_subscriber(os.getenv("REGISTER_TOPIC"), lambda msg: on_register_message(session, msg, publisher3))
+    register_rfid_subscriber = setup_subscriber(os.getenv("REGISTER_RFID_TOPIC"), lambda msg: on_register_rfid_message(session, msg, publisher4))
 
     rfid_thread = threading.Thread(target=run_subscriber, args=(rfid_subscriber,))
     ultrasonic_thread = threading.Thread(target=run_subscriber, args=(ultrasonic_subscriber,))
     dashboard_thread = threading.Thread(target=run_subscriber, args=(register_subscriber,))
+    register_rfid_thread = threading.Thread(target=run_subscriber, args=(register_rfid_subscriber,))
 
     rfid_thread.start()
     ultrasonic_thread.start()
     dashboard_thread.start()
+    register_rfid_thread.start()
 
     rfid_thread.join()
     ultrasonic_thread.join()
     dashboard_thread.join()
+    register_rfid_thread.start()
 
     session.close()
 
